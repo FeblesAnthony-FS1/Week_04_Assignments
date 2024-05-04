83a02b78ca8a55aa734b28cdc2a530c52a7b47f3
Febles-FS <149134772+FeblesAnthony-FS1@users.noreply.github.com>
2024-05-04 04:23:58 -0500

    Update script.js

--- Assignment-Employee_Tracker/script.js
+++ Assignment-Employee_Tracker/script.js
@@ -19,7 +19,7 @@ class PartTime extends Employee {
   calculatePay() {
     const weeks = 52;
     const annualPay = this.payRate * this.hours * weeks;
-    this.annualSalary = annualPay - 1000;
+    this.annualSalary = annualPay;
   }
 }
 
@@ -46,6 +46,14 @@ class Main {
     ];
   }
 
+  static displayMenu() {
+    console.log("Main Menu:");
+    console.log("1. Add Employee");
+    console.log("2. Remove Employee");
+    console.log("3. Edit Employee");
+    console.log("4. Display Employees");
+  }
+
   addEmployee() {
     const input = prompt(
       "Enter name, age, pay rate, and hours per week (separated by commas):"
@@ -69,9 +77,34 @@ class Main {
 
   removeEmployee() {
     const choice = parseInt(prompt("Enter the employee number to remove:"));
-    const employee = this.employees.find((emp, index) => index + 1 === choice);
+    this.employees.splice(choice - 1, 1);
+    this.displayEmployees();
+  }
+
+  editEmployee() {
+    const choice = parseInt(prompt("Enter the employee number to edit:"));
+    const newPayRate = parseFloat(prompt("Enter the new pay rate:"));
+    this.employees[choice - 1].payRate = newPayRate;
+    this.displayEmployees();
+  }
+
+  displayEmployees() {
+    console.log("ID\tName\tSalary\thrs\tpay\tFT/PT");
+    this.employees.forEach((employee, index) => {
+      console.log(
+        `${index + 1}\t${employee.name}\t${employee.annualSalary}\t${
+          employee.hours
+        }\t${employee.payRate}\t${employee.employeeType}`
+      );
+    });
   }
 }
 
-// Create an instance of the Main class
-const main = new Main();
+(function () {
+  const main = new Main();
+  Main.displayMenu();
+  main.addEmployee();
+  main.removeEmployee();
+  main.editEmployee();
+  main.displayEmployees();
+})();
